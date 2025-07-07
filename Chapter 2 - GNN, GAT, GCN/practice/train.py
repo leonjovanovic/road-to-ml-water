7aad08211b93c0eefeafe8a3fa0f4f4985139e66
Leon Jovanovic <leonjovanovic1996@gmail.com>
2025-07-07 22:36:49 +0200

    Add support for GAT and GIN

--- Chapter 2 - GNN, GAT, GCN/practice/train.py
+++ Chapter 2 - GNN, GAT, GCN/practice/train.py
@@ -1,4 +1,6 @@
+import random
 from matplotlib import pyplot as plt
+import numpy as np
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
     classification_report,
@@ -8,27 +10,43 @@ from torch import Tensor
 import torch
 import torch.nn as nn
 from torch.optim import Adam
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GINConv, GATConv, GATv2Conv, MessagePassing
 import torch.nn.functional as F
 from torch_geometric.data import Data
 from torch_geometric.datasets import Planetoid
 from tqdm import tqdm
 
 
-class GCNModel(nn.Module):
+torch.manual_seed(42)
+random.seed(42)
+np.random.seed(42)
+
+
+class GNN(nn.Module):
+    """
+    A Graph Neural Network (GNN) model that consists of multiple layers of message passing.
+    IMPORTANT: Use it only for GCN or GAT layers, not for GINConv.
+    """
+
     def __init__(
-        self, in_channels: int, hidden_channels: list[int], output_channels: int
+        self,
+        in_channels: int,
+        hidden_channels: list[int],
+        output_channels: int,
+        layer_class: MessagePassing,
+        dropout_rate: float = 0.5,
     ):
-        super(GCNModel, self).__init__()
+        super(GNN, self).__init__()
+        self.dropout_rate = dropout_rate
         self.layers = nn.ModuleList()
 
-        layer_first = GCNConv(in_channels, hidden_channels[0])
+        layer_first = layer_class(in_channels, hidden_channels[0])
         self.layers.append(layer_first)
 
         for i in range(len(hidden_channels) - 1):
-            self.layers.append(GCNConv(hidden_channels[i], hidden_channels[i + 1]))
+            self.layers.append(layer_class(hidden_channels[i], hidden_channels[i + 1]))
 
-        layer_last = GCNConv(hidden_channels[-1], output_channels)
+        layer_last = layer_class(hidden_channels[-1], output_channels)
         self.layers.append(layer_last)
 
     def forward(self, data: Data):
@@ -36,18 +54,93 @@ class GCNModel(nn.Module):
         for layer in self.layers[:-1]:
             x = layer(x, edge_index)
             x = F.relu(x)
-            x = F.dropout(x)
+            x = F.dropout(x, p=self.dropout_rate, training=self.training)
+        x = self.layers[-1](x, edge_index)
+        return x
+
+
+class GIN(nn.Module):
+    """
+    A Graph Isomorphism Network (GIN) model that consists of multiple layers of GINConv.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: list[int],
+        output_channels: int,
+        dropout_rate: float = 0.5,
+    ):
+        super(GIN, self).__init__()
+        self.dropout_rate = dropout_rate
+
+        self.layers = nn.ModuleList()
+
+        layer_first = GINConv(
+            self.make_mlp(in_channels, hidden_channels[0]), train_eps=True
+        )
+        self.layers.append(layer_first)
+
+        for i in range(len(hidden_channels) - 1):
+            self.layers.append(
+                GINConv(
+                    self.make_mlp(hidden_channels[i], hidden_channels[i + 1]),
+                    train_eps=True,
+                )
+            )
+
+        layer_last = GINConv(
+            self.make_mlp(hidden_channels[-1], output_channels), train_eps=True
+        )
+        self.layers.append(layer_last)
+
+    def make_mlp(
+        self,
+        in_dim: int,
+        out_dim: int,
+        hidden_dim: int | None = None,
+        use_bn: bool = False,
+    ) -> nn.Sequential:
+        if hidden_dim is None:
+            hidden_dim = out_dim
+        layers = [nn.Linear(in_dim, hidden_dim)]
+        if use_bn:
+            layers.append(nn.BatchNorm1d(hidden_dim))
+        layers += [nn.ReLU(), nn.Linear(hidden_dim, out_dim)]
+        return nn.Sequential(*layers)
+
+    def forward(self, data: Data):
+        x, edge_index = data.x, data.edge_index
+        for layer in self.layers[:-1]:
+            x = layer(x, edge_index)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout_rate, training=self.training)
         x = self.layers[-1](x, edge_index)
         return x
 
 
 def train(
-    model: GCNModel, data: Data, hyperparameters: dict
+    model: GNN | GIN, data: Data, hyperparameters: dict
 ) -> tuple[list[float], list[float]]:
+
+    eps_params = [p for n, p in model.named_parameters() if n.endswith("eps")]
+    other_params = [p for n, p in model.named_parameters() if not n.endswith("eps")]
+
+    wd = 0 if model is GIN else 5e-4
+    wd_esp = 5e-4
     optimizer = Adam(
-        model.parameters(),
-        lr=hyperparameters["learning_rate"],
-        weight_decay=hyperparameters["weight_decay"],
+        [
+            {
+                "params": other_params,
+                "lr": hyperparameters["learning_rate"],
+                "weight_decay": wd,
+            },
+            {
+                "params": eps_params,
+                "lr": hyperparameters["learning_rate"] * 0.1,
+                "weight_decay": wd_esp,
+            },
+        ]
     )
 
     train_losses, val_losses = [], []
@@ -99,7 +192,7 @@ def plot_losses(train_losses: list[float], val_losses: list[float]):
     plt.show()
 
 
-def evaluate(model: GCNModel, data: Data):
+def evaluate(model: GNN, data: Data):
     model.eval()
     out: Tensor = model(data)
 
@@ -115,16 +208,27 @@ def evaluate(model: GCNModel, data: Data):
     plt.show()
 
 
-def main(hyperparameters: dict):
+def main(layer_class: MessagePassing, hyperparameters: dict):
+
     cora_dataset = Planetoid(root="data", name="Cora")
     data = cora_dataset[0]
-    data.to(hyperparameters["device"])
+    data = data.to(hyperparameters["device"])
 
-    model = GCNModel(
-        in_channels=data.num_features,
-        hidden_channels=hyperparameters["layers"],
-        output_channels=cora_dataset.num_classes,
-    )
+    if layer_class in [GCNConv, GATConv, GATv2Conv]:
+        model = GNN(
+            in_channels=data.num_features,
+            hidden_channels=hyperparameters["layers"],
+            output_channels=cora_dataset.num_classes,
+            layer_class=layer_class,
+            dropout_rate=hyperparameters.get("dropout", 0.5),
+        )
+    else:
+        model = GIN(
+            in_channels=data.num_features,
+            hidden_channels=hyperparameters["layers"],
+            output_channels=cora_dataset.num_classes,
+            dropout_rate=hyperparameters.get("dropout", 0.5),
+        )
     model.to(hyperparameters["device"])
 
     train_losses, val_losses = train(model, data, hyperparameters)
@@ -132,14 +236,3 @@ def main(hyperparameters: dict):
     plot_losses(train_losses, val_losses)
 
     evaluate(model, data)
-
-
-if __name__ == "__main__":
-    hyperparameters = {
-        "learning_rate": 0.01,
-        "weight_decay": 5e-4,
-        "epochs": 200,
-        "log_interval": 10,
-        "device": "cuda" if torch.cuda.is_available() else "cpu",
-    }
-    main(hyperparameters)
